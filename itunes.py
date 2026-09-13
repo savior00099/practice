ff9a0a2b5451091dc9c38092f37b1ef9ce24f100
savior00099 <savior00099@users.noreply.github.com>
2026-09-13 23:21:29 +0600

    your message here

--- itunes.py
+++ itunes.py
@@ -9,4 +9,4 @@ if len(sys.argv) != 2:
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
 o = response.json()
 for result in o["results"]:
-    print(result["trackName"] + " by " + result["artistName"])
+    print(result["trackName"] + " by " = result["artistName"])
