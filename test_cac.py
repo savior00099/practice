0e62674a8becb6fecb70d6eb25044976ebeb17c2
savior00099 <savior00099@users.noreply.github.com>
2026-09-15 03:32:48 +0600

    added test_cac.py

--- test_cac.py
+++ test_cac.py
@@ -4,8 +4,15 @@ def main():
     test_square()
 
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 square was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 square was not 9")        
+
 
 if __name__ == "__main__":
     main()
