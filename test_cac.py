4b8c1900def476ef9c0af5b50e0bf2d6a57f6918
savior00099 <savior00099@users.noreply.github.com>
2026-09-15 09:39:38 +0600

    added test_cac.py

--- test_cac.py
+++ test_cac.py
@@ -1,38 +1,11 @@
 from calculator import square
 
-def main():
-    test_square()
-
 def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 square was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 square was not 9")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("-2 square was not 4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 square was not 9")                    
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
 
 
 
